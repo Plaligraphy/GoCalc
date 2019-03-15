79d0bc0d87fa66de52e01a2f8c0edeef9895a227
Evan Carter <32000073+evanac21@users.noreply.github.com>
2019-03-14 18:32:21 -0700

    init

---
+++ calc.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	var in int
+	color.Magenta("TI-84 Emulator")
+	fmt.Println("1. Graph")
+	fmt.Println("2. Math")
+	fmt.Println("3. Settings")
+	fmt.Println("4. Exit")
+	fmt.Scanln(&in)
+	switch in {
+	case 1:
+		graphingFunction()
+	case 2:
+		mathFunction()
+	case 3:
+		var vnum = "0.1"
+		color.Red("TI-84 Emulator")
+		color.Yellow("Version: " + vnum)
+		color.Yellow("Created by Evan Carter 2019")
+	case 4:
+	}
+}
+func graphingFunction() {
+
+}
+func mathFunction() {
+
+}
